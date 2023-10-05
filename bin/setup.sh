ee92d8e22d30ab0c07b0d30aecf001a08c73560e
munuhee <murichustephen@gmail.com>
2023-10-05 20:49:59 +0300

    create setup.sh

---
+++ bin/setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "🚀 Setting up sales-and-inventory-management"
+
+echo "🔨 Building Docker image..."
+docker build -t sales-and-inventory-management:1.0 .
+
+if [ $? -ne 0 ]; then
+  echo "❌ Error: Docker image build failed!"
+  exit 1
+fi
+
+echo "🚀 Starting Docker containers in detached mode..."
+docker run -d -p 8000:8000 sales-and-inventory-management:1.0
+
+if [ $? -ne 0 ]; then
+  echo "❌ Error: Docker container failed to start!"
+  exit 1
+fi
+
+echo "✔️ Setup completed successfully!"
